11c2795bc25b75c98f78994e40153dbf43a5509e
hiranokazuma <kazuma.hirano@aimnext.com>
2024-10-05 22:29:11 +0900

    rename project

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/WeatherAnalysis/', // リポジトリ名をベースパスに設定
+  base: '/WeatherApp/', // リポジトリ名をベースパスに設定
   build: {
     sourcemap: false, // ソースマップを無効化
   },
